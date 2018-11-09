ba0c6dface7b49ca51833bc32642e3b95affd1e8
Avery Johnson <avery627@gmail.com>
2018-11-09 15:37:37 -0500

    Updated Auto Announcer (Every 2 Minutes).
    Auto Announcer now uses 2 minute intervals along with randomly choosing an announcement. It also has built in statistics such as bans, kicks, warns, and etc.

--- autoAnnounce.php
+++ autoAnnounce.php
@@ -4,7 +4,7 @@
  * Plugin Name: autoAnnounce
  * Version: 0.0.1
  * Details: Auto announces messages every set amount of minutes.
- * Created Date: Tuesday, April 17th 2018, 9:26:00 pm
+ * Created Date: Monday, April 17th 2018, 9:26:00 pm
  * Author: Avery Johnson
  *
  * Copyright (c) 2018 Avery Johnson
@@ -14,25 +14,47 @@ class autoAnnounce
 {
     public static function cronCalled()
     {
-        // AUTO ANNOUNCE CONFIG
-        // TO ADD BREAKS EACH MINUTE ADD BLANK LINES. I'M TOO LAZY TO ADD A WAIT SYSTEM. IF SOMEONE WANTS TO PR IT ON GITHUB GO FOR IT
+        // RANDOM AUTO ANNOUNCE CONFIG
+        
+        if (date('i') % 2 == 0) {
+            exit();
+        }
+
+        $warns = 0;
+        $kicks = 0;
+        $bans = 0;
+        $commends = 0;
+        $playtime = 0;
+        $players = 0;
+        $staff = 0;
+
+        foreach (dbquery('SELECT * FROM warnings') as $warn) {$warns++;}
+        foreach (dbquery('SELECT * FROM kicks') as $kick) {$kicks++;}
+        foreach (dbquery('SELECT * FROM bans') as $ban) {$bans++;}
+        foreach (dbquery('SELECT * FROM commend') as $commend) {$commends++;}
+        foreach (dbquery('SELECT * FROM players') as $playedtime) {
+            $playtime = $playtime + $playedtime['playtime'];
+            $players++;
+        }
+
+        $playtime = $playtime / 60;
+
+        foreach (dbquery('SELECT * FROM users WHERE rank <> "user"') as $user) {$staff++;}
+
         $config['messages'] = [
-            'This server is ^2powered^0 by ^2FiveM Administration System^0. To learn more visit https://GitHub.com/CADOJRP',
-            'Don\'t forget to register on our forums at ^2forum.FiveM.net',
-            '^1E^2X^3A^4M^5P^6L^7E ^8M^9E^0S^1S^2A^3G^4E',
+            '^2' . $players . '^0 unique players and counting!',
+            '^2' . $playtime . '^0 total hours played on CADOJRP!',
+            '^2' . $warns . '^0 warnings, ^2' . $kicks . '^0 kicks, ^2' . $bans . '^0 bans, and ^2' . $commends . '^0 commendations!',
+            '^2' . $staff . '^0 total staff members working hard!',  
+            'Have a suggestion for the server? Feel free to post on the forums at ^2www.CADOJRP.com^0 or in the Discord.',      
+            'Think you have what it takes to be apart of our ^2Staff Team^0? Apply at ^2www.CADOJRP.com',
+            'Looking to join a department? Join TeamSpeak at ^2ts.cadojrp.com^0 or apply on our website at ^2www.CADOJRP.com^0.',
+            'Roleplaying as a ^2civilian^0? You must use out MDT/CAD at ^2http://www.cad-cadojrp.ga/^0. Failure to do so may result in a warning, kick, or ban.',
+            'Make sure to read our rules at ^2https://rules.cadojrp.com^0. Failure to follow the rules may result in a warning, kick, or ban.',
+            'Want to talk to other civilians? Join our TeamSpeak at ^2ts.CADOJRP.com',
+            'Want to stay up to date? Join our ^4Discord^0 server (^2https://discord.gg/a9uPTQM^0). Our ^4Discord^0 is used for text communication only!',
         ];
 
-        if (!isset($_SESSION['runcount'])) {
-            $_SESSION['runcount'] = 0;
-        } else {
-            if(!empty($config['messages'][$_SESSION['runcount']])) {
-                sendMessage($config['messages'][$_SESSION['runcount']]);
-            }
-            if ($_SESSION['runcount'] >= count($config['messages']) - 1) {
-                $_SESSION['runcount'] = 0;
-            } else {
-                $_SESSION['runcount']++;
-            }
-        }
+        sendMessage($config['messages'][array_rand($config['messages'])]);
     }
 }
