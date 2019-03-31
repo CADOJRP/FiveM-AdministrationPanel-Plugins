c2bfe1c8f04e3dc11f819309ad80e700cf652f45
timnboys <timnboys@users.noreply.github.com>
2019-03-31 16:46:52 -0400

    Uploaded AntiVACBanned File

--- antivacbanned.php
+++ antivacbanned.php
@@ -2,10 +2,10 @@
 
 class antivacbanned
 {
-    public static function playerJoined($license)
+    public static function playerJoined($license,$name)
 	{
 		$userinfo = dbquery('SELECT * FROM players WHERE license="' . escapestring($license) . '"');
-        	$steamid = hexdec(strtoupper(str_replace('steam:', '', $userinfo[0]['steam'])));
+        $steamid = hexdec(strtoupper(str_replace('steam:', '', $userinfo[0]['steam'])));
 		$steaminfo = json_decode(file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerBans/v1/?key=" . $GLOBALS['apikey'] . '&format=json&steamids=' . $steamid),true);
 		if ($steaminfo['players'][0]['NumberOfVACBans'] >= 1 || $steaminfo['players'][0]['VACBanned'] == 1 || $steaminfo['players'][0]['NumberOfGameBans'] >= 1 || $steaminfo['players'][0]['CommunityBanned'] >= 1)
 		{
